580f9ea8fcf0dce06b9ce52083dee99c59e8815e
aquashie14 <amakushi17@gmail.com>
2025-03-29 14:18:59 +0000

    Add Post model and update create_post endpoint to accept post data

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
+from fastapi import Body
+from pydantic import BaseModel
+from typing import Optional
+
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating:Optional[float] = None
 
 @app.get("/")
 def root():
@@ -10,5 +20,7 @@ def get_posts():
     return {"data": "List of posts"}
 
 @app.post("/createposts")
-def create_post():
-    return {"message": "Post created successfully"}
+def create_post(new_posts: Post):
+    print(new_posts)
+    print(new_posts.dict())
+    return {"data": new_posts}
