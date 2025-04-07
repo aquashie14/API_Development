b80ae3fe08efc61e75c528c37e8dfae2290aee02
aquashie14 <amakushi17@gmail.com>
2025-04-07 11:08:58 +0000

    Implement CRUD operations for posts with FastAPI

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import Body
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
+
 app = FastAPI()
 
 class Post(BaseModel):
@@ -10,6 +11,8 @@ class Post(BaseModel):
     content: str
     published: bool = True
     rating:Optional[float] = None
+
+
 
 my_posts =[{"title": "title of post 1", "content": "content of post 1", "id":1},{"title": "title of post 2", "content": "content of post 2", "id":2}]
 
@@ -45,9 +48,20 @@ def get_post(id: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} was not found")
     return {"post_details": post}
 
-@app.delete("/posts/{id}")
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
     index = find_index(id)
+    if index==None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} does not exist")
     my_posts.pop(index)
-    return {"message": "Post was deleted"}
-     
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id: int, post:Post):
+    index = find_index(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} does not exist")
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return{"data": "post_dict"}
